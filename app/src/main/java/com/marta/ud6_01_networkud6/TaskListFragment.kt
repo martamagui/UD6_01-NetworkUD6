ef9bdabb285e36408c77e00f0efb36d681b8ead7
Marta Molina <marta.molina@live.u-tad.com>
2022-01-24 19:28:18 +0100

    Initial Commit

---
+++ app/src/main/java/com/marta/ud6_01_networkud6/TaskListFragment.kt
@@ -0,0 +1,25 @@
+package com.marta.ud6_01_networkud6
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class TaskListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
+    }
+}
