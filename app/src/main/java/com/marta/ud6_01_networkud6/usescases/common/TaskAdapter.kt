14f7b33d160366cffa6f277ce6d1e47c61fa4f42
Marta Molina <marta.molina@live.u-tad.com>
2022-01-28 19:32:57 +0100

    TaskAdapter

--- app/src/main/java/com/marta/ud6_01_networkud6/usescases/common/TaskAdapter.kt
+++ app/src/main/java/com/marta/ud6_01_networkud6/usescases/common/TaskAdapter.kt
@@ -3,26 +3,30 @@ package com.marta.ud6_01_networkud6.usescases.common
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.marta.ud6_01_networkud6.databinding.ItemTaskBinding
 import com.marta.ud6_01_networkud6.databinding.ItemTasklistBinding
 import com.marta.ud6_01_networkud6.model.Task
 import com.marta.ud6_01_networkud6.model.TaskList
 import com.marta.ud6_01_networkud6.usescases.common.TaskListAdapter.ViewHolder
+import androidx.recyclerview.widget.ListAdapter
 
 
+class TaskAdapter: ListAdapter<Task, TaskAdapter.ViewHolderTask>(TaskItemCallBack()) {
 
-class TaskAdapter: ListAdapter<Task, ViewHolder>(TaskItemCallBack()) {
-    inner class ViewHolder(val binding: ItemTaskBinding): RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskListAdapter.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskAdapter.ViewHolderTask {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding: ItemTaskBinding  = ItemTaskBinding.inflate(inflater, parent, false)
+        return ViewHolderTask(binding)
     }
 
-    override fun onBindViewHolder(holder: TaskListAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: TaskAdapter.ViewHolderTask, position: Int) {
+        val task = getItem(position)
+        holder.binding.tvTask.text = task.title
     }
+    inner class ViewHolderTask( val binding: ItemTaskBinding): RecyclerView.ViewHolder(binding.root)
+
+
 }
 class TaskItemCallBack: DiffUtil.ItemCallback<Task>(){
     override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
