fb3d5907f4b5d05c42488b18966c88e2cbd83f25
Marta Molina <marta.molina@live.u-tad.com>
2022-01-30 21:13:51 +0100

    Delete TaskBody.kt

--- app/src/main/java/com/marta/ud6_01_networkud6/model/TaskBody.kt
+++
@@ -1,9 +0,0 @@
-package com.marta.ud6_01_networkud6.model
-
-
-class TaskBody ( val taskId: Int,
-                 val listIdFk: Int,
-                 val description: String,
-                 val state: String,
-                 val title: String){
-}
